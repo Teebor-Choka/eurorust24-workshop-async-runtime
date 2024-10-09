fa75226fa541e8470789a03b576f45d480ce1c63
Tibor Csóka <csokat@gmail.com>
2024-10-09 14:49:43 +0200

    Assignment #3: reimplement async mutex functionality

--- projects/03_mutex/src/main.rs
+++ projects/03_mutex/src/main.rs
@@ -1,16 +1,108 @@
 use std::{
-    cell::UnsafeCell,
-    ops::{Deref, DerefMut},
-    sync::Arc,
-    time::Duration,
+    cell::UnsafeCell, collections::BTreeMap, future::Future, ops::{Deref, DerefMut}, sync::Arc, task::{Poll, Waker}, time::Duration
 };
+
+struct WakerQueue {
+    counter: u64,
+    unlocked: bool,
+    wakers: BTreeMap<u64,Option<Waker>>
+}
+
+impl WakerQueue {
+    fn new() -> Self {
+        Self {
+            unlocked: true,
+            counter: 0,
+            wakers: BTreeMap::new()
+        }
+    }
+}
 
 struct AsyncMutex<T> {
     data: UnsafeCell<T>,
+    wakers: std::sync::Mutex<WakerQueue>
+}
+
+impl<T> AsyncMutex<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            data: UnsafeCell::new(value),
+            wakers: std::sync::Mutex::new(WakerQueue::new())
+        }
+    }
+
+    pub async fn lock(&self) -> AsyncMutexGuard<'_, T> {
+        let key = {
+            let mut queue = self.wakers.lock().unwrap();
+
+            if queue.unlocked {
+                queue.unlocked = false;
+                return AsyncMutexGuard { mutex: &self };
+            }
+
+            let key = queue.counter;
+            queue.counter += 1;
+            queue.wakers.insert(key, None);
+            
+            // bug in Rust, must be scope dropped
+            // drop(queue);
+            key
+        };
+
+        Acquire { mutex: self, key, done: false }.await
+    }
+}
+
+struct Acquire<'a, T> {
+    key: u64,
+    mutex: &'a AsyncMutex<T>,
+    done: bool
+}
+
+impl<'a, T> Future for Acquire<'a, T> {
+    type Output = AsyncMutexGuard<'a, T>;
+
+    fn poll(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+        let mut queue = self.mutex.wakers.lock().unwrap();
+
+        let Some(waker) = queue.wakers.get_mut(&self.key) else {
+            self.done = true;
+            return Poll::Ready(AsyncMutexGuard { mutex: self.mutex });
+        };
+
+        *waker = Some(cx.waker().clone());
+        Poll::Pending
+    }
+}
+
+impl<'a, T> Drop for Acquire<'a, T> {
+    fn drop(&mut self) {
+        if self.done {
+            return ;
+        }
+
+        let mut queue = self.mutex.wakers.lock().unwrap();
+        if queue.wakers.remove(&self.key).is_none() {
+            drop(AsyncMutexGuard { mutex: self.mutex });
+        }
+    }
 }
 
 struct AsyncMutexGuard<'a, T> {
     mutex: &'a AsyncMutex<T>,
+}
+
+impl<'a, T> Drop for AsyncMutexGuard<'a, T> {
+    fn drop(&mut self) {
+        let mut queue = self.mutex.wakers.lock().unwrap();
+        if let Some((_, waker)) = queue.wakers.pop_first() {
+            if let Some(waker) = waker {
+                waker.wake();
+            }
+        } else {
+            queue.unlocked = true;
+        }
+    }
 }
 
 // Safety: We are a mutex :)
@@ -33,8 +125,8 @@ impl<T> DerefMut for AsyncMutexGuard<'_, T> {
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    // let mutex = Arc::new(AsyncMutex::new(0));
-    let mutex = Arc::new(tokio::sync::Mutex::new(0));
+    let mutex = Arc::new(AsyncMutex::new(0));
+    // let mutex = Arc::new(tokio::sync::Mutex::new(0));
 
     let mutex1 = mutex.clone();
     tokio::spawn(async move {
